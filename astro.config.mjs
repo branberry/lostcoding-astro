a61808e975637deef42fb84dbbe7029cd85e2866
Brandon Ly <brandonly@lostcoding.com>
2024-03-10 12:46:08 -0500

    add slot

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc()]
+  integrations: [react(), markdoc()],
+  experimental: {
+    contentCollectionCache: true,
+  },
 });
--- markdoc.config.ts
+++ markdoc.config.ts
@@ -2,6 +2,14 @@ import { component, defineMarkdocConfig } from "@astrojs/markdoc/config";
 
 export default defineMarkdocConfig({
   tags: {
+    card: {
+      render: component("./src/components/Card.astro"),
+      attributes: {
+        title: { type: "String" },
+        body: { type: "String" },
+        href: { type: "String" },
+      },
+    },
     test: {
       render: component("./src/components/react/Test.tsx", "Test"),
     },
